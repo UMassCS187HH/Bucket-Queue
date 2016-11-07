608640e782187d558587ad761050fd638959eb38
jagath-jaikumar <jjaikumar@wpi.edu>
2016-11-07 10:18:00 -0500

    added patientOrgan class

---
+++ support/driver/PatientOrgan.java
@@ -0,0 +1,13 @@
+package driver;
+
+public class PatientOrgan {
+	String name;
+	String organ;
+	String condition;
+	
+	public PatientOrgan(String name, String organ, String condition) {
+		this.name = name;
+		this.organ = organ;
+		this.condition = condition;
+	}
+}
